a632f150a91e556c3134a0a8cd493992f9fba0b9
noobie <150881131+noobieoncrack@users.noreply.github.com>
2024-11-05 00:29:49 -0600

    added more more stuff

--- app/src/main/java/cookie/generator/Window.java
+++ app/src/main/java/cookie/generator/Window.java
@@ -22,10 +22,11 @@ public class Window implements ActionListener
     ImageIcon mainTitlePicture = new ImageIcon("app\\src\\main\\resources\\pictures\\mainpic.png");
     JLabel mainDisplay = new JLabel(mainTitlePicture);
     JLabel mainText = new JLabel("hi there want cookie click button");
+    JLabel secondText = new JLabel("whoever you are, i think youre awesome");
 
     Window()
     {
-        addCookieButton.setBounds(100, 300, 200, 50);
+        addCookieButton.setBounds(100, 275, 200, 50);
         addCookieButton.setFocusable(false);
         addCookieButton.addActionListener(this);
         addCookieButton.setBackground(Color.PINK);
@@ -34,11 +35,14 @@ public class Window implements ActionListener
         mainDisplay.setLocation(100, 65);
 
         mainText.setBounds(110, 0, 200, 100);
+        secondText.setBounds(90, 290, 250, 100);
         
         mainFrame.add(mainDisplay);
         mainFrame.add(addCookieButton);
         mainFrame.add(mainText);
+        mainFrame.add(secondText);
 
+        mainFrame.getContentPane().setBackground(Color.WHITE);
         mainFrame.setIconImage(cookieTitlePicture.getImage());
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainFrame.setSize(400,400);
@@ -50,7 +54,7 @@ public class Window implements ActionListener
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        
+
         if(e.getSource() == addCookieButton)
         {
             playSound("heavenly.wav");
@@ -69,7 +73,7 @@ public void playSound(String nameSound)
     }
     catch(Exception error)
     {
-        System.out.println("There was an error playing the sound.");
+        System.out.println("There was an error playing the sound. Whoopsie...");
         error.printStackTrace();
     }
 
